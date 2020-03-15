8120b7e036e5efbc41b389967533ffe02327b676
ainurapolatoglu <ainura.dyuss@gmail.com>
2020-03-15 21:48:04 +0100

    Fixed closing application only when user chooses to quit, otherwise keep asking options. #10
    Fixed format of Show Task List option. Each task in the list prints in new line. #11
    Initial implementation of File class to write/read tasks to file. #5

--- src/main/java/com/sda/todolist/ToDoApp.java
+++ src/main/java/com/sda/todolist/ToDoApp.java
@@ -1,5 +1,11 @@
 package com.sda.todolist;
 
+import java.io.FileNotFoundException;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.io.ObjectOutputStream;
+import java.nio.file.Path;
+import java.nio.file.Paths;
 import java.text.ParseException;
 import java.text.SimpleDateFormat;
 import java.util.ArrayList;
@@ -8,43 +14,81 @@ import java.util.List;
 import java.util.Scanner;
 
 public class ToDoApp {
+
+    private static final String filepath = "ToDoList.txt";
     private static final SimpleDateFormat dateFormat = new SimpleDateFormat("dd/MM/yyyy");
 
-    public static void main(String[] args) throws ParseException {
-
-        System.out.println("Welcome to ToDoList");
-        System.out.println("You have X tasks todo and Y tasks are done!");
-        System.out.println("Pick an option:");
-        System.out.println("(1) Show Task List (by date or project)");
-        System.out.println("(2) Add New Task");
-        System.out.println("(3) Edit Task (update, mark as done, remove)");
-        System.out.println("(4) Save and Quit");
+    public static void main(String[] args) throws ParseException, IOException {
 
         List<Task> myTaskList = new ArrayList<>();
-
         Scanner in = new Scanner(System.in);
+        int option = 0;
+        System.out.println("Welcome to ToDoList");
+        System.out.println("You have X tasks todo and Y tasks are done!");
 
-        int option = Integer.parseInt(in.nextLine()); //input from user
-        if (option == 1) {
-            myTaskList.add(new Task("meet psycologist ", new Date(2020, 02, 16), "project1  ", false)); //Adding object in arraylist
-            myTaskList.add(new Task("meet psycologist ", new Date(2020, 02, 16), "project1  ", false)); //Adding object in arraylist
-            System.out.println(myTaskList);
-        } else if (option == 2) {
-            System.out.println("Give me the name of the task:");
-            String name = in.nextLine();
+        do {
+            System.out.println("Pick an option:");
+            System.out.println("(1) Show Task List (by date or project)");
+            System.out.println("(2) Add New Task");
+            System.out.println("(3) Edit Task (update, mark as done, remove)");
+            System.out.println("(4) Save and Quit");
+            option = Integer.parseInt(in.nextLine()); //input from user
 
-            System.out.println("Give me the due date in format dd/MM/yyyy:");
-            String date = in.nextLine();
-            Date dueDate = dateFormat.parse(date);
+            if (option == 1) {
+                myTaskList.forEach(System.out::println);
+            } else if (option == 2) {
+                System.out.println("Give me the name of the task:");
+                String name = in.nextLine();
 
-            System.out.println("Give me the name of the project:");
-            String project = in.nextLine();
+                System.out.println("Give me the due date in format dd/MM/yyyy:");
+                String date = in.nextLine();
+                Date dueDate = dateFormat.parse(date);
 
-            Task newTask = new Task(name, dueDate, project, false);
-            myTaskList.add(newTask);
-            System.out.println("New task has been added to the ToDo List:");
-            System.out.println(myTaskList);
-        }
+                System.out.println("Give me the name of the project:");
+                String project = in.nextLine();
+
+                Task newTask = new Task(name, dueDate, project, false);
+                myTaskList.add(newTask);
+
+                System.out.println("Your new task is:");
+                System.out.println(newTask);
+
+            } else if (option == 3) {
+                System.out.println("Edit option was chosen");
+            } else if (option == 4) {
+                System.out.println(" Save and Quit option was chose!");
+                System.out.println(" Good Bye!");
+                break;
+            } else {
+                System.out.println("Entered option doesn't exist. Please enter correct option!");
+            }
+        } while (option > 0);
+
+
+
+           /* FileOutputStream fileOut = new FileOutputStream(filepath);
+            ObjectOutputStream objectOut = new ObjectOutputStream(fileOut);
+            try {
+                objectOut.writeObject(newTask.toString());
+                System.out.println("New task was successfully added to a ToDo List");
+
+            } catch (Exception ex) {
+                ex.printStackTrace();
+            }
+            finally {
+                objectOut.close();
+            }
+            */
+
+        // String input = FileUtil.readTextFile(filepath);
+        // System.out.println(input);
+        //FileUtil.writeToTextFile("ToDoList.txt", myTaskList.toString());
+
+        // System.out.println(FileUtil.readTextFile("Task"));
+
+        //FileUtil.readTextFileByLines("file.txt");
+        //Path path = Paths.get("file.txt");
         in.close();
+
     }
 }
